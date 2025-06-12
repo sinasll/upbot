e7b4e8a6c0d535a4f2d15e6ca814fc6bf4c982a4
sinasll <sinasalah6@gmail.com>
2025-06-12 14:27:54 +0300

    no discount

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     basic: { name: '+0.2× Mining Power — 40⭐️', price: 40, description: 'Upgrade your mining power by +0.2×' },
     advanced: { name: '+0.4× Mining Power — 80⭐️', price: 80, description: 'Upgrade your mining power by +0.4×' },
     recommended: { name: '+0.6× Mining Power — 120⭐️', price: 120, description: 'Upgrade your mining power by +0.6×' },
-    ultra: { name: '+0.8× Mining Power — 160⭐️', price: 1600, description: 'Upgrade your mining power by +0.8×' },
+    ultra: { name: '+0.8× Mining Power — 160⭐️', price: 160, description: 'Upgrade your mining power by +0.8×' },
     ultimate: { name: '+1.0× Mining Power — 200⭐️', price: 200, description: 'Upgrade your mining power by +1.0×' }
   },
   MESSAGES: {
